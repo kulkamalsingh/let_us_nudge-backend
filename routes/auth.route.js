835d10c4e4991585d38e1d79b44b5ed37bfdf12a
Kulkamal Singh <kulkamalsingh377@gmail.com>
2025-05-17 00:01:00 +0530

    fix: Update routes in auth.route.js for consistency and clarity

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -2,11 +2,11 @@ import express from 'express';
 import { Login, sendotp, Signup, Signupcustomer, verifyotp } from '../controllers/auth.controller.js';
 const router = express.Router();
 
-router.post('/signup',Signup);
+router.post('/business/register',Signup);
 router.post('/login',Login);
-router.post('/sendotp',Signupcustomer)
+router.post('/customer/register/send-otp',Signupcustomer)
 router.post('/send-otp',sendotp)
-router.post('/verify-otp',verifyotp)
+router.post('/customer/register/verify-otp',verifyotp)
 
 
 export default router;
